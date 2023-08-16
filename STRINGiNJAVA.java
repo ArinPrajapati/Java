b24d1d233cf7d7a4a8b0519803c0d2853c3dffaf
Arin Prajapati <arinprajapati78@gmail.com>
2023-08-16 09:29:42 +0530

    strings

---
+++ STRINGiNJAVA.java
@@ -0,0 +1,17 @@
+public class STRINGiNJAVA {
+    public static void main(String[] args) {
+        // string = a reference data type that can store one or more characters
+        //  refernce data types have access to usefull methods
+
+        String name = "Arin";
+//        boolean result = "arin".equalsIgnoreCase(name);
+//        int result = name.length();
+
+//        char result = name.charAt(0);
+//        int result = name.indexOf('A');
+//        boolean result = name.isEmpty();
+        
+        System.out.println(result);
+
+    }
+}
