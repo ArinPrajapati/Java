0e451f5c99ecbb5bb12cff16127d442ecaf68138
Arin Prajapati <arinprajapati78@gmail.com>
2023-08-18 09:55:01 +0530

    final keyword

---
+++ finaljavaKeyWord.java
@@ -0,0 +1,11 @@
+public class finaljavaKeyWord {
+//    final keyword anything  in the keyword cannot be changed in program
+public static void main(String[] args) {
+    final double pi = 3.4423;
+//    pi = 44.65435;
+    //run and you will understand
+    System.out.println(pi);
+
+}
+
+}
